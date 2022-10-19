b8ec7c6e860aefa38c560ee2a9ae496bc9d57593
isma-91 <isma91@hotmail.it>
2022-10-19 15:20:46 +0200

    First commit with all prompts and some styles

---
+++ JS/scrypt.js
@@ -0,0 +1,8 @@
+let userName = prompt("Qual è il tuo nome?");
+
+let userSurname = prompt("Qual è il tuo cognome?");
+
+let userColor = prompt("Qual è il tuo colore preferito?");
+
+document.getElementById("dati").innerHTML =
+  userName + userSurname + userColor + "22";
